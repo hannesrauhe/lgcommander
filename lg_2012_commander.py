bca127c35eabc0481ce10ed6842e28c8f16993f0
Hannes Rauhe <hannes.rauhe@sap.com>
2017-02-12 11:33:25 +0100

    * removing GUI stuff to reuse for scripting

--- lg_2012_commander.py
+++ lg_2012_commander.py
@@ -1,33 +1,15 @@
 #!/usr/bin/env python3
 
 import http.client
-from tkinter import *
 import xml.etree.ElementTree as etree
 import socket
 import re
 import sys
 lgtv = {}
+
 dialogMsg =""
 headers = {"Content-Type": "application/atom+xml"}
-lgtv["pairingKey"] = "DDGWAF"
-
-class MyDialog:
-    def __init__(self, parent, dialogMsg):
-        top = self.top = Toplevel(parent)
-        Label(top, text = dialogMsg, justify="left").pack()
-        self.e = Entry(top)
-        self.e.pack(padx=5)
-        self.e.focus_set()
-        b = Button(top, text="Ok", command=self.ok)
-        b.pack(pady=5)
-        top.bind("<Return>", self.ok)
-        top.title("Lg Commander")
-        top.geometry("410x280+10+10")
-    def ok(self,dummy=None):
-        global result
-        result = self.e.get()
-        self.top.destroy()
-
+lgtv["pairingKey"] = "000000"
 
 def getip():
     strngtoXmit =   'M-SEARCH * HTTP/1.1' + '\r\n' + \
@@ -84,13 +66,9 @@ def getSessionid():
 
 def getPairingKey():
     displayKey()
-    root = Tk()
-    root.withdraw()
     dialogMsg = "Please enter the pairing key\nyou see on your TV screen\n"
-    d = MyDialog(root, dialogMsg)
-    root.wait_window(d.top)
+    result = input(dialogMsg)
     lgtv["pairingKey"] = result
-    d.top.destroy()
 
 def handleCommand(cmdcode):
     conn = http.client.HTTPConnection( lgtv["ipaddress"], port=8080)
@@ -123,18 +101,11 @@ dialogMsg += "Success in establishing command session\n"
 dialogMsg += "=" * 28 + "\n"
 dialogMsg += "Enter command code i.e. a number between 0 and 1024\n"
 dialogMsg += "Enter a number greater than 1024 to quit.\n"
-dialogMsg += "Some useful codes (not working with 2012 models):\n"
-dialogMsg += "for EZ_ADJUST     menu enter   255 \n"
-dialogMsg += "for IN START        menu enter   251 \n"
-dialogMsg += "for Installation     menu enter   207 \n"
-dialogMsg += "for POWER_ONLY mode enter   254 \n"
-dialogMsg += "Warning: do not enter 254 if you \ndo not know what POWER_ONLY mode is. "
 
 
 result = "91"
 while int(result) < 1024:
-    root = Tk()
-    root.withdraw()
-    d = MyDialog(root, dialogMsg)
-    root.wait_window(d.top)
+    result = input(dialogMsg)
     handleCommand(result)
+
+sys.exit(0)
